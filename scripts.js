496b0fbdd79ce5ef7eceb7420c04bdc174689cb7
Ken Nguyen <aznboikenny@yahoo.com>
2020-02-06 01:15:51 -0600

    added animation to show/hide add form

--- scripts.js
+++ scripts.js
@@ -1,4 +1,27 @@
 //document ready
 $(function() {
-  
+
+  let addFormButton = $('.add-form-btn');
+  let addFormCancel = $('.add-form-cancel');
+
+  //add form button
+  addFormButton.click(function() {
+    //hide add button
+    $(this).addClass('hide');
+    //show cancel button
+    $(this).next().removeClass('hide');
+    //show add new form
+    $(this).next().next().css({width: '100%', height: '500px'});
+  })
+
+  //cancel add form button
+  addFormCancel.click(function(){
+    //hide cancel button
+    $(this).addClass('hide');
+    //show add button
+    $(this).prev().removeClass('hide');
+    //collapse add new form
+    $(this).next().css({width: 0, height: 0});
+  })
+
 });
